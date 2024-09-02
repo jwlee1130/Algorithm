bde6d0b7929cbf1fd9ed183bc5d1436fa5446573
jwlee1130 <49229253+jwlee1130@users.noreply.github.com>
2024-09-02 19:28:03 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 6.47 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최댓값 만들기 (2) - 120862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862#) 
 
 ### 성능 요약
 
-메모리: 75.3 MB, 시간: 0.14 ms
+메모리: 66.3 MB, 시간: 6.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 02일 19:21:29
+2024년 09월 02일 19:28:02
 
 ### 문제 설명
 
--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,5 +1,8 @@
+import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
+        
+        /*
         int answer = numbers[0]*numbers[1];
         int max =0;
         for(int i=0; i<numbers.length-1; i++){
@@ -11,6 +14,11 @@ class Solution {
             }
             
         }
-        return answer;
+        
+        */
+        // arrays.sort 보고 내가 다시 생각한 풀이 
+        Arrays.sort(numbers);
+        return Math.max(numbers[0]*numbers[1],
+            numbers[numbers.length-1]*numbers[numbers.length-2]);
     }
 }
