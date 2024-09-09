f1703f50bf9cae293681d8f5752a7ce3a43c0ef1
jwlee1130 <49229253+jwlee1130@users.noreply.github.com>
2024-09-09 09:31:48 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.29 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/README.md
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 숨어있는 숫자의 덧셈 (2) - 120864 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120864) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120864#) 
 
 ### 성능 요약
 
-메모리: 75.1 MB, 시간: 13.73 ms
+메모리: 72.7 MB, 시간: 0.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 09일 09:25:18
+2024년 09월 09일 09:31:46
 
 ### 문제 설명
 
--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -4,6 +4,7 @@ class Solution {
         //흠 for문 돌리고 대문자 65~92 97~ 125 해당하는거 패스하고
         //아닐때 값 곱하기*10*k으로 이어지면 10 100을 곱해서 자릿수를 정해준다.
         //그리고 문자가 나오면 answer에 값을 더하고 값을 넣어줄 변수를 초기화혹 k=0으로 초기화해서 다시 1의자리부터 시작하게 한다.
+       /*
         String str="";
         for(int i=0; i < my_string.length(); i++){
             if(65<= my_string.charAt(i)+0 && my_string.charAt(i)+0<=125){
@@ -18,6 +19,14 @@ class Solution {
         }
         if(!str.equals("")){
             answer+=Integer.valueOf(str);
+        }
+        */
+        // 정규식 으로 푸는 해결방법을 보고 다시 생각해보면서 해보기
+        
+        String[] str = my_string.replaceAll("[a-z,A-Z]"," ").split(" ");
+        for(String s : str){
+            if(!s.equals(""))
+                answer+=Integer.valueOf(s);
         }
         return answer;
     }
