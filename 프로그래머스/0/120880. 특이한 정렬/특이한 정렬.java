84eba6f60d012b73168f33245446272f4a188e18
jwlee1130 <49229253+jwlee1130@users.noreply.github.com>
2024-09-10 20:06:15 +0900

    [level 0] Title: 특이한 정렬, Time: 0.78 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        int[] answer = new int[numlist.length];
+        int[][] num = new int[numlist.length][2];
+        
+        for(int i=0; i<num.length; i++){
+                num[i][0]=numlist[i];
+                num[i][1]=Math.abs(numlist[i]-n);
+            
+        }
+        
+        Arrays.sort(num,new Comparator<int[]>(){
+                
+                public int compare(int[] o1,int[] o2){
+                    //절대값 같다. 거리 같을떄
+                    if(o1[1] != o2[1] ){
+                       return Integer.compare(o1[1],o2[1]);
+                    }else{
+                        return Integer.compare(o2[0],o1[0]);
+                    }
+                    
+                    
+                }
+            
+        
+        });
+        
+        for(int i=0; i<num.length; i++){
+           answer[i]=num[i][0];
+        }
+        
+        return answer;
+    }
+}
