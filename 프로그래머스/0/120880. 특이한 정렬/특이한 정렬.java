050e909a4f9dbb5f7bf9816e4eb891530af7529c
jwlee1130 <49229253+jwlee1130@users.noreply.github.com>
2024-09-10 20:10:04 +0900

    [level 0] Title: 특이한 정렬, Time: 0.68 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -12,19 +12,17 @@ class Solution {
         }
         
         Arrays.sort(num,new Comparator<int[]>(){
-                
                 public int compare(int[] o1,int[] o2){
-                    //절대값 같다. 거리 같을떄
+                    //거리 다를 때 절대값 크면 그냥 큰수
                     if(o1[1] != o2[1] ){
                        return Integer.compare(o1[1],o2[1]);
                     }else{
+                        //절대값 같다. 거리 같을떄 내림차순 
                         return Integer.compare(o2[0],o1[0]);
                     }
                     
                     
                 }
-            
-        
         });
         
         for(int i=0; i<num.length; i++){
