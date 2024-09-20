1dccc8c148f3e616c9279afad2092fca049ac195
jwlee1130 <49229253+jwlee1130@users.noreply.github.com>
2024-09-20 12:38:13 +0900

    [level 0] Title: 문자열 밀기, Time: 2.33 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,17 +1,20 @@
 class Solution {
     public int solution(String A, String B) {
-        int len = A.length();
-        int count =0;
-        String compare = A;
+//         int len = A.length();
+//         int count =0;
+//         String compare = A;
         
-        for(int i=0; i <len; i++){
-            if(compare.equals(B)) return count;
+//         for(int i=0; i <len; i++){
+//             if(compare.equals(B)) return count;
             
-            String end = A.substring((len-1)-i);
-            compare = end + A.substring(0,(len-1)-i);
-            count++;
+//             String end = A.substring((len-1)-i);
+//             compare = end + A.substring(0,(len-1)-i);
+//             count++;
             
-        }
-        return -1;
+//         }
+//         return -1;
+        //좋은 풀이 어차피 B를 이어 붙이면 A에 해당하는 문자열이 나올 것이고
+        //indexOf로 첫 해당하는 문자열에 위치를 알려줘서 그걸 리턴한다.
+        return (B+B).indexOf(A);
     }
 }
