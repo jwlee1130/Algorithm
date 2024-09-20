accde9abd1e7765521e5da134aebccbcf4c05be6
jwlee1130 <49229253+jwlee1130@users.noreply.github.com>
2024-09-20 12:07:10 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -3,8 +3,8 @@ class Solution {
     public int solution(int[] common) {
         int lastCheck = common[1]-common[0];
         
-        for(int i=0; i<common.length-1; i++){
-            int check = common[i+1]-common[i];
+        for(int i=common.length-1; i>0; i--){
+            int check = common[i]-common[i-1];
             
             if(check!=lastCheck){
                 return common[common.length-1]*(common[1]/common[0]);
