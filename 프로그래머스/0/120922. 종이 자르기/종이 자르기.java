473368c19a65f26a3ca1c667c057159653dcbb91
jwlee1130 <49229253+jwlee1130@users.noreply.github.com>
2024-09-20 10:47:41 +0900

    [level 0] Title: 종이 자르기, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -1,16 +1,17 @@
 class Solution {
     public int solution(int M, int N) {
-        int answer = 0;
-        if(M==1 && N== 1){
-            answer = 0;
-        }else if(M==1){
-            answer+=N-1;
-        }else if(N==1){
-            answer+=M-1;
-        }else{
-            answer=M*N-1;
-        }
+        // int answer = 0;
+        // if(M==1 && N== 1){
+        //     answer = 0;
+        // }else if(M==1){
+        //     answer+=N-1;
+        // }else if(N==1){
+        //     answer+=M-1;
+        // }else{
+        //     answer=M*N-1;
+        // }
+        // 풀이 어차피 1x1해도 -1 해서 0되서 굳이 안해도 됨
         
-        return answer;
+        return M*N-1;
     }
 }
